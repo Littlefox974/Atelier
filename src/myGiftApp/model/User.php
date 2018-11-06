461788cf06dd71a56895fd699f97317c041a2589
Unknown <omarjalilfierro@gmail.com>
2018-11-06 08:59:30 +0100

    Model User

---
+++ src/myGiftApp/model/User.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jalil
+ * Date: 11/5/18
+ * Time: 5:13 PM
+ */
+
+namespace myGiftApp\model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model {
+
+    protected $table = 'MGB_user';
+    protected $primaryKey = 'id';
+
+    public function orders(){
+        return self::hasMany('MGB_order','idUser');
+    }
+
+}
