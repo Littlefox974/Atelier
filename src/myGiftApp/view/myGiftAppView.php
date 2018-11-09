ea11280b460e683bba534999823fd4fa827c3b16
Ruben Conde <rubenconde_mag96@hotmail.com>
2018-11-09 08:12:39 +0100

    Button Logout

--- src/myGiftApp/view/myGiftAppView.php
+++ src/myGiftApp/view/myGiftAppView.php
@@ -35,9 +35,11 @@ class myGiftAppView extends AbstractView
         $httpReq = new HttpRequest();
         $routeCart = $router->urlFor('cart');
         $routeProfile = $router->urlFor('profile');
+        $routeLogout = $router->urlFor('logout');
         return "<div class='headercatalogue'>
                     <h3>Prestations</h3>
                     <a href='$routeProfile'> <img src=\"$httpReq->root"."html/img/profile.svg\"> </a>
+                    <a href='$routeLogout'> <img src=\"$httpReq->root" . "html/img/remove.svg\"> </a>
                     <a href='$routeCart'>
                         <img src=\"$httpReq->root" . "html/img/cart.svg\">
                     </a>
